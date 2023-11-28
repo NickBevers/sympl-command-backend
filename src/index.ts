493b0cd62ef9c35601f3573ff92e3446deb3cb3e
Nick Bevers <nick.bevers9@gmail.com>
2023-11-28 16:27:58 +0100

    feat: add cors to the index.ts file; update tha pp.use statements to include express.json and express.urlencoded

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import SearchResult from './routes/searchResults';
-dotenv.config();
+import cors from "cors";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
-mongoose.connect(process.env.MONGO_DB_URL!)
+mongoose.connect(process.env.MONGO_DB_URL!);
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', SearchResult)
+app.use('/api', SearchResult);
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
